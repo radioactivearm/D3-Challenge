a299da0818e8b0ec8de317cc590da54e6c686dcd
arm <andy.mcrae21@gmail.com>
2021-04-25 14:28:43 -0500

    made a chart area

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,43 @@
 console.log('loaded app.js');
+
+// This is to find the width of the container so I can set
+// the width of the scatter plot to be the same
+var scatter = d3.select('#scatter');
+// console.log(head.text());
+var scatterWidth = scatter.property('offsetWidth');
+console.log(scatterWidth);
+
+
+// I am going to make a graph
+
+// ==========================================================
+// Starting with the svg and chart group
+// This beginning part is a Chocolate cake recipe I am pulling from
+// class examples
+
+// I may need to tweak these later to make it look good
+var svgWidth = scatterWidth;
+var svgHeight = 3 * scatterWidth / 5; 
+
+var margin = {
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50
+};
+
+var height = svgHeight - margin.top - margin.bottom;
+var width = svgWidth - margin.left - margin.right;
+
+var svg = d3.select('#scatter')
+    .append('svg')
+    .attr('height', svgHeight)
+    .attr('width', svgWidth);
+
+var chartGroup = svg.append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+// ==================================================================
+
+
+
